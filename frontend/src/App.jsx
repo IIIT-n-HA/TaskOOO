c4110a6f17d30ca4417c891e13d02e378b5293fe
BullMonk <harshsmhs2003@gmail.com>
2024-07-02 14:04:13 +0530

    Connecting the backend with the frontend. Using redux for quality state management.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./pages/home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import AllTask from "./pages/AllTask";
 import ImportantTask from "./pages/ImportantTask";
 import CompletedTask from "./pages/CompletedTask";
 import IncompletedTask from "./pages/IncompletedTask";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  // const navigate = useNavigate();
+  // const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  // useEffect(() => {
+  //   if (isLoggedIn === false) {
+  //     navigate("/login");
+  //   }
+  // }, []);
+
   return (
     <div className="bg-gray-900 text-black p-2 h-screen relative">
       <Router>
